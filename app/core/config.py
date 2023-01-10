6754c3493830022c0ed20c0411aac8ae098483f3
Кирилл Резник <sunlight_1091@mail.ru>
2023-01-11 00:18:31 +0200

    QR hw ver 1 fixed config default values

--- app/core/config.py
+++ app/core/config.py
@@ -3,9 +3,9 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
 
-    app_title: str
+    app_title: str = 'Благотворительный проект QR-кот'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
-    secret: str
+    secret: str = 'very_secret_key_555'
 
     class Config:
         env_file = '.env'
